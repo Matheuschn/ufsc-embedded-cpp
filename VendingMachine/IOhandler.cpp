663e130fba3a8227516b49ef9680527948d03aa2
Pordesu <pedropordeuss@gmail.com>
2023-10-04 10:33:08 -0300

    Modificações no  IOhandler

--- VendingMachine/IOhandler.cpp
+++ VendingMachine/IOhandler.cpp
@@ -12,69 +12,48 @@ StateMachine::Inputs IOhandler::getInput(){
     unsigned int input = *entradas;
     if (memory != input){
         memory = input;
-        if (input == SW0){
-            return StateMachine::Inputs::M025;
-        }
-        else {
-            if (input == SW1){
+        switch (input){
+            case SW0:
+                return StateMachine::Inputs::M025;
+            case SW1:
                 return StateMachine::Inputs::M050;
-            }
-            else{
-                if (input == SW2){
-                    return StateMachine::Inputs::M100;
-                }
-                else{
-                    if (input == SW3){
-                        return StateMachine::Inputs::DEV;
-                    }
-                    else{
-                        if (input == SW4){
-                            return StateMachine::Inputs::MEET;
-                        }
-                        else{
-                            if (input == SW5){
-                                return StateMachine::Inputs::ETIRPS;
-                            }
-                            else{
-                                return StateMachine::Inputs::NONE;
-                            }
-                        }
-                    }
-                }
-            }
+            case SW2:
+                return StateMachine::Inputs::M100;
+            case SW3:
+                return StateMachine::Inputs::DEV;
+            case SW4:
+                return StateMachine::Inputs::MEET;
+            case SW5:
+                return StateMachine::Inputs::ETIRPS;
+            default:
+                return StateMachine::Inputs::NONE;
         }
-    }
-    else {
-        return StateMachine::Inputs::NONE;
+        
     }
 }
 
 void IOhandler::writeOutputs(std::vector <StateMachine::Outputs> output){
+    oledClear();
     for (int i =0; i < output.size(); i++){
         switch(output[i]){
             case StateMachine::Outputs::D025 :
-                oledClear();
-                setLine(0);
+                setLine(i);
                 printString((char*)"D025");
                 break;
             case StateMachine::Outputs::D050 :
-                oledClear();
-                setLine(0);
+                setLine(i);
                 printString((char*)"D050");
                 break;
             case StateMachine::Outputs::D100 :
-                oledClear();
-                setLine(0);
+                setLine(i);
                 printString((char*)"D100");
                 break;
             case StateMachine::Outputs::LMEET :
-                oledClear();
-                setLine(0);
+                setLine(i);
                 printString((char*)"LMEET");
                 break;
             case StateMachine::Outputs::LETIRPS:
-                oledClear();
-                setLine(0);
+                setLine(i);
                 printString((char*)"LETIRPS");
                 break;        
         }
